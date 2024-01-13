7b518b11d750924af97faa14b87332cd185b7d3e
undefined <johann@objekt.stream>
2024-01-13 02:14:41 +0100

    ~

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -4,6 +4,10 @@ var Bullet = preload("res://Bullet.tscn")
 
 
 func _process(_delta):
+	var closest_enemy = get_closest_enemy()
+	if closest_enemy != null:
+		rotate_towards_enemy(closest_enemy)
+	
 	if Input.is_action_just_pressed("action_shoot"):
 		spawn_bullet()
 
@@ -12,9 +16,25 @@ func spawn_bullet():
 	var bullet = Bullet.instantiate()
 	add_child(bullet)
 
-	#ugly
-	var sprite: Sprite2D = $GunSprite
 	var muzzle: Marker2D = $GunSprite/Muzzle
 	var pos = muzzle.global_position
-	var rot = sprite.rotation + randf_range(-0.1, 0.1) # spread
+	var rot = self.rotation + randf_range(-0.1, 0.1) # spread
 	bullet.fire(pos, rot)
+
+func get_closest_enemy():
+	var closest_enemy = null
+	var closest_distance = INF
+
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		var distance = global_position.distance_to(enemy.global_position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+
+	return closest_enemy
+
+
+func rotate_towards_enemy(enemy):
+	var direction = enemy.global_position - global_position
+	self.rotation = direction.angle()
+	$GunSprite.flip_v = direction.x < 0
--- scripts/Tracer.gd
+++ scripts/Tracer.gd
@@ -3,7 +3,7 @@ extends Line2D
 @export var MAX_POINTS = 20
 @onready var curve := Curve2D.new()
 
-func _process(_delta):
+func _physics_process(_delta):
 	curve.add_point(get_parent().position)
 	if curve.get_point_count() > MAX_POINTS:
 		curve.remove_point(0)
