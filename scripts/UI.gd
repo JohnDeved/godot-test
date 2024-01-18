5580271c516c71b991a98203dfea8e86304f6d96
undefined <johann@objekt.stream>
2024-01-18 05:53:42 +0100

    ~ pause on levelup

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -4,3 +4,7 @@ extends CanvasLayer
 
 func _on_player_xp_gained(_amount:int, progress:float) -> void:
 		xp_bar.value = progress * 100
+
+func _on_player_level_up(_level:int) -> void:
+	xp_bar.value = 100
+	get_tree().paused = true
