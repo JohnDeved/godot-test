af9192f8621adacc74bc85bbe867bdd1c13ee9a8
undefined <johann@objekt.stream>
2024-01-18 06:28:17 +0100

    + ui

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
-@onready var xp_bar: ProgressBar = $XPBar
+@onready var xp_bar: ProgressBar = %XPBar
+@onready var skill_selection: HBoxContainer = %SkillSelection
+
+func _ready() -> void:
+	skill_selection.hide()
 
 func _on_player_xp_gained(_amount:int, progress:float) -> void:
 		xp_bar.value = progress * 100
@@ -8,3 +12,4 @@ func _on_player_xp_gained(_amount:int, progress:float) -> void:
 func _on_player_level_up(_level:int) -> void:
 	xp_bar.value = 100
 	get_tree().paused = true
+	skill_selection.show()
